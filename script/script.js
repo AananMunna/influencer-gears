b89a1f3080383590799f8ff6f98ba8e94ea125f6
unknown <aananmunna420@gmail.com>
2025-01-19 15:11:18 +0600

    adding mobile nav for mobile

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function menu(){
+    let navLinks = document.getElementById('navLinks');
+    navLinks.style.display='flex'
+}
+function closeIcon(){
+    let navLinks = document.getElementById('navLinks');
+    navLinks.style.display='none'
+}
